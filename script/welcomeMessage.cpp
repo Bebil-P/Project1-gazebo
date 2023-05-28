da0d170f7dc98fc7bbd96cb7bd458585fb14acc5
Bebil-P <Bebil-P@users.noreply.github.com>
2023-05-28 11:24:22 +0100

    Initial commit

---
+++ script/welcomeMessage.cpp
@@ -0,0 +1,22 @@
+#include <gazebo/gazebo.hh>
+#include <string.h>
+namespace gazebo
+{
+  class WorldPluginMyRobot : public WorldPlugin
+  {
+    public: WorldPluginMyRobot() : WorldPlugin()
+            {
+		std::string name;
+              	std::cout<<("Welcome to Bebz World!\n");
+	      	std::cout<<("Greetings\n");
+ 		std::cout<<("whats your name: \n");
+		std::cin >> name;
+		std::cout<< "Welcome " <<name << "\n";
+            }
+
+    public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
+            {
+            }
+  };
+  GZ_REGISTER_WORLD_PLUGIN(WorldPluginMyRobot)
+}
